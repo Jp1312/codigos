5e3390002820003c28aea89e62a94e9e04657bd2
Jp1312 <126490477+Jp1312@users.noreply.github.com>
2023-05-28 21:42:32 -0300

    Add files via upload

--- trabalhoAC.py
+++ trabalhoAC.py
@@ -1,19 +1,68 @@
 #funcoes
-def desenhatabuleiro(n1):
-    tir = 0;
-    ac = 0;
-    print(f"JOGADOR 1  :   {n1}\n")
-    print(f"Tiros:  {tir}   Acertos: {ac}   \n")
-    print("   1  2  3  4  5")
-    for x in range(5):  #
-        print(x + 1, end=": ")
-        for c in range(5):
-            print(0,end="  ")
-        print("\n")
 
+def imprimir_matriz(matriz):
+    for i, linha in enumerate(matriz):
+        print(f"{i+1}  ", end='')
+        for elemento in linha:
+            print(elemento, end=' ')
+        print()
+def jogo(n1,n2,subs1,subs2):
+    tir = 0;ac = 0;p1 = 0;p2 = 0;
+    tab1 = [[0, 0, 0, 0, 0] for _ in range(5)]; tab2 = [[0, 0, 0, 0, 0] for _ in range(5)];
 
+    while p1 < 3 and p2 < 3:
+        # Jogador 1
+        print(f'Jogador 1: {n1}, pressione ENTER para continuar')
+        input()
+        print(f"JOGADOR 1: {n1}")
+        print(f"Tiros: {tir}   Acertos: {ac}\n")
+        print("   1 2 3 4 5")
+        print()
+        imprimir_matriz(tab1);
+        l1 = int(input("Qual linha? : "))
+        c1 = int(input("Qual coluna? : "))
+        temp = (l1, c1)
+        if temp in subs2:
+            print("ACERTOU!")
+            tir += 1;ac += 1; p1 += 1;
+            tab1[l1-1][c1-1] = 'S';
 
+        else:
+            print("ERROU!");
+            tir += 1;
+            tab1[l1-1][c1-1] = 'X';
 
+        if p1 >= 3:
+            break
+
+        # Jogador 2
+        print("\n" * 8);
+        print(f'Jogador 2: {n2}, pressione ENTER para continuar')
+        input()
+        print(f"JOGADOR 2: {n2}")
+        print(f"Tiros: {tir}   Acertos: {ac}\n")
+        print("   1 2 3 4 5")
+        print()
+        imprimir_matriz(tab2);
+        l2 = int(input("Qual linha? : "))
+        c2 = int(input("Qual coluna? : "))
+        temp = (l2, c2)
+        if temp in subs1:
+            print("ACERTOU!")
+            tir += 1;ac += 1;p2 += 1
+            tab2[l1-1][c1-1] = 'S'
+        else:
+            print("ERROU!")
+            tir += 1
+            tab2[l1-1][c1-1] = 'X'
+
+        if p2 >= 3:
+            break
+
+    if p1 >= 3:
+        print(f"{n1} venceu o jogo!")
+    else:
+        print(f"{n2} venceu o jogo!")
 #bloco principal
 subs1 = []
 subs2= []
@@ -22,22 +71,21 @@ n1 = input("Nome do jogador 1: ")
 print("\n")
 
 for c in range(3):
-    print(f'SUBMARINO {c+1} \n')
+    print(f'submarino {c+1}:\n')
 
     while True:
-        lin = int(input(f"Linha (1 a 5): "))
-        col = int(input(f"Coluna (1 a 5): "))
+        lin = int(input("Linha:"))
+        col = int(input(f"Coluna:"))
 
         if lin < 1 or lin > 5 or col < 1 or col > 5:
             print("\nLinha e coluna devem estar entre 1 e 5. Tente novamente.\n")
         else:
             break
 
-    subs1.append((lin, col))
+    tup=(lin,col)   #tupla com as coordenadas do submarino
+    subs1.append(tup);
     print('\n')
 
-    subs1.append((lin, col))
-    print('\n')
 
 print('\n' * 10)
 
@@ -56,17 +104,19 @@ for c in range(3):
         else:
             break
 
-    subs2.append((lin, col))
+
+    tup = (lin, col)
+    subs2.append(tup);
     print('\n')
 
-print(f'Jogador 1: {n1}, pressione ENTER para continuar')
-input()
 
-print('\n' * 10)
-
-#desenhando o quadrado
+jogo(n1,n2,subs1,subs2)
 
 
 
-desenhatabuleiro(n1)
+
+
+
+
+
 
