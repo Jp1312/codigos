cb230477506acab175a7a2af5d5a0445c3503e47
Jp1312 <126490477+Jp1312@users.noreply.github.com>
2023-05-29 20:30:20 -0300

    Add files via upload

--- trabalhoAC.py
+++ trabalhoAC.py
@@ -1,3 +1,7 @@
+#João Pedro Rodrigues Bezamat, matricula - 202301216109
+#Pedro Henrique Vodovoz de Brito, matricula - 202301172969
+#Jorge Felippe Leal Magarao, matricula - 202301284422
+
 #funcoes
 
 def imprimir_matriz(matriz):
@@ -12,6 +16,7 @@ def jogo(n1,n2,subs1,subs2):
 
     while p1 < 3 and p2 < 3:
         # Jogador 1
+        print("\n" * 18)
         print(f'Jogador 1: {n1}, pressione ENTER para continuar')
         input()
         print(f"JOGADOR 1: {n1}")
@@ -19,9 +24,16 @@ def jogo(n1,n2,subs1,subs2):
         print("   1 2 3 4 5")
         print()
         imprimir_matriz(tab1);
+        print();
         l1 = int(input("Qual linha? : "))
         c1 = int(input("Qual coluna? : "))
         temp = (l1, c1)
+
+        while tab1[l1 - 1][c1 - 1] in ("S", "X"):
+            print("Posição já atirada. Escolha novamente:\n")
+            l1 = int(input("Qual linha? : "))
+            c1 = int(input("Qual coluna? : "))
+
         if temp in subs2:
             print("ACERTOU!")
             tir += 1;ac += 1; p1 += 1;
@@ -32,11 +44,13 @@ def jogo(n1,n2,subs1,subs2):
             tir += 1;
             tab1[l1-1][c1-1] = 'X';
 
+        imprimir_matriz(tab1)
+        input()
         if p1 >= 3:
             break
 
         # Jogador 2
-        print("\n" * 8);
+        print("\n" * 18);
         print(f'Jogador 2: {n2}, pressione ENTER para continuar')
         input()
         print(f"JOGADOR 2: {n2}")
@@ -44,17 +58,28 @@ def jogo(n1,n2,subs1,subs2):
         print("   1 2 3 4 5")
         print()
         imprimir_matriz(tab2);
+        print();
         l2 = int(input("Qual linha? : "))
         c2 = int(input("Qual coluna? : "))
         temp = (l2, c2)
+
+        while tab2[l2 - 1][c2 - 1] in ("S", "X"):
+            print("Posição já atirada. Escolha novamente:\n")
+            l2 = int(input("Qual linha? : "))
+            c2 = int(input("Qual coluna? : "))
+
         if temp in subs1:
             print("ACERTOU!")
             tir += 1;ac += 1;p2 += 1
             tab2[l1-1][c1-1] = 'S'
+
         else:
             print("ERROU!")
             tir += 1
             tab2[l1-1][c1-1] = 'X'
+
+        imprimir_matriz(tab2)
+        input()
 
         if p2 >= 3:
             break
@@ -63,54 +88,63 @@ def jogo(n1,n2,subs1,subs2):
         print(f"{n1} venceu o jogo!")
     else:
         print(f"{n2} venceu o jogo!")
-#bloco principal
-subs1 = []
-subs2= []
+#bloco principaljp
 
-n1 = input("Nome do jogador 1: ")
-print("\n")
+while True:
+    subs1 = []
+    subs2 = []
 
-for c in range(3):
-    print(f'submarino {c+1}:\n')
+    n1 = input("Nome do jogador 1: ")
+    print("\n")
 
-    while True:
-        lin = int(input("Linha:"))
-        col = int(input(f"Coluna:"))
+    for c in range(3):
+        print(f'submarino {c + 1}:\n')
 
-        if lin < 1 or lin > 5 or col < 1 or col > 5:
-            print("\nLinha e coluna devem estar entre 1 e 5. Tente novamente.\n")
-        else:
-            break
+        while True:
+            lin = int(input("Linha:"))
+            col = int(input("Coluna:"))
 
-    tup=(lin,col)   #tupla com as coordenadas do submarino
-    subs1.append(tup);
-    print('\n')
+            if lin < 1 or lin > 5 or col < 1 or col > 5:
+                print("\nLinha e coluna devem estar entre 1 e 5. Tente novamente.\n")
+            else:
+                tup = (lin, col)  # tupla com as coordenadas do submarino
 
+                if tup in subs1:
+                    print("Submarino já existe nessa posição. Escolha outra.")
+                else:
+                    subs1.append(tup)
+                    break
+        print('\n')
 
-print('\n' * 10)
+    print('\n' * 20)
 
-n2 = input("Nome do jogador 2: ")
-print("\n")
+    n2 = input("Nome do jogador 2: ")
+    print("\n")
 
-for c in range(3):
-    print(f'SUBMARINO {c+1} \n')
+    for c in range(3):
+        print(f'submarino {c + 1}:\n')
 
-    while True:
-        lin = int(input(f"Linha (0 a 5): "))
-        col = int(input(f"Coluna (0 a 5): "))
+        while True:
+            lin = int(input("Linha:"))
+            col = int(input("Coluna:"))
 
-        if lin < 1 or lin > 5 or col < 1 or col > 5:
-            print("\nOs números de linha e coluna devem estar entre 1 e 5. Tente novamente.\n")
-        else:
-            break
+            if lin < 1 or lin > 5 or col < 1 or col > 5:
+                print("\nLinha e coluna devem estar entre 1 e 5. Tente novamente.\n")
+            else:
+                tup = (lin, col)  # tupla com as coordenadas do submarino
 
+                if tup in subs2:
+                    print("Submarino já existe nessa posição. Escolha outra.")
+                else:
+                    subs2.append(tup)
+                    break
+        print('\n')
 
-    tup = (lin, col)
-    subs2.append(tup);
-    print('\n')
-
-
-jogo(n1,n2,subs1,subs2)
+    jogo(n1, n2, subs1, subs2)
+    repete = int(input("Deseja jogar novamente?(1 sim/2 nao):"))
+    if repete != 1:
+        print("fim de jogo.")
+        break
 
 
 
